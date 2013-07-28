6eda9f1baf095a2f3a65c4b8cc5f658e23881837
touchmenot <balaji.sampath10@gmail.com>
2013-07-28 22:02:34 +0000

    Displaying data from the first object in the database

--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from testapp.models import Question
 
 def index(request):
     """
@@ -12,5 +13,10 @@ def index(request):
     as soon as it finds one that matches it calls the callback python
     function by passing an HttpRequest object as the first arg.
     """
-    return HttpResponse("Hello World!, This is my first view!")
+    
+    questions = Question.objects.all()
+    response_string = "Questions <br />"
+    response_string += '<br/>'.join(["id: %s, subject: %s" 
+                                      % (q.id, q.subject) for q in questions])
+    return HttpResponse(response_string)
 
